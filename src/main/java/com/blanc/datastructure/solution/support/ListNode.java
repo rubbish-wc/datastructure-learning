8a1191fdbd736f49217d8ff801a20f69ad7b6629
w_bl <w_bl@ypsxm.net>
2019-05-21 09:49:00 +0800

    leetcode

---
+++ src/main/java/com/blanc/datastructure/solution/support/ListNode.java
@@ -0,0 +1,10 @@
+package com.blanc.datastructure.solution.support;
+
+/**
+ * @author wangbaoliang
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
