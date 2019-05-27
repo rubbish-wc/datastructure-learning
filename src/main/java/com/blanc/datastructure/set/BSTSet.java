5b79017659eac0c2ca520b82bf6b7b62a187fe16
w_bl <w_bl@ypsxm.net>
2019-05-27 14:26:34 +0800

    集合

--- src/main/java/com/blanc/datastructure/set/BSTSet.java
+++ src/main/java/com/blanc/datastructure/set/BSTSet.java
@@ -17,26 +17,48 @@ public class BSTSet<E extends Comparable<E>> implements Set<E> {
         bst = new BST<>();
     }
 
+    /**
+     * 添加元素,不可添加重复的元素
+     * @param e
+     */
     @Override
     public void add(E e) {
         bst.add(e);
     }
 
+    /**
+     * 删除一个元素
+     * @param e
+     * @return
+     */
     @Override
     public E remove(E e) {
         return bst.remove(e);
     }
 
+    /**
+     * 判断是否包含
+     * @param e
+     * @return
+     */
     @Override
     public boolean contains(E e) {
         return bst.contains(e);
     }
 
+    /**
+     * 判断是否为空
+     * @return
+     */
     @Override
     public boolean isEmpty() {
         return bst.isEmpty();
     }
 
+    /**
+     * 获取集合的大小
+     * @return
+     */
     @Override
     public int getSize() {
         return bst.size();
