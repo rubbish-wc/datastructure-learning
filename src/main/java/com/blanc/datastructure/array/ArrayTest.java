b6914ae3abaacfb12252ae7e9e8e1e6628b001ba
王保良 <1479930060@qq.com>
2019-12-13 10:04:48 +0800

    数组代码提交

--- src/main/java/com/blanc/datastructure/array/ArrayTest.java
+++ src/main/java/com/blanc/datastructure/array/ArrayTest.java
@@ -6,24 +6,31 @@ package com.blanc.datastructure.array;
  * @author blanc
  */
 public class ArrayTest {
+
     public static void main(String[] args) {
-        Array<Integer> array = new Array<>(20);
-        for (int i = 0; i < 10; i++) {
-            array.addLast(i);
+//        Array<Integer> array = new Array<>(20);
+//        for (int i = 0; i < 10; i++) {
+//            array.addLast(i);
+//        }
+//        System.out.println(array);
+//
+//        array.add(1, 100);
+//        System.out.println(array);
+//        array.addFirst(-1);
+//        System.out.println(array);
+//
+//        array.remove(2);
+//        System.out.println(array);
+//        array.removeElement(4);
+//        System.out.println(array);
+//        array.removeFirst();
+//        System.out.println(array);
+//
+        Integer[] integers = new Integer[1];
+        integers[0] = 1;
+        for (Integer a : integers) {
+            System.out.println(a);
         }
-        System.out.println(array);
-
-        array.add(1, 100);
-        System.out.println(array);
-        array.addFirst(-1);
-        System.out.println(array);
-
-        array.remove(2);
-        System.out.println(array);
-        array.removeElement(4);
-        System.out.println(array);
-        array.removeFirst();
-        System.out.println(array);
     }
 
 }
