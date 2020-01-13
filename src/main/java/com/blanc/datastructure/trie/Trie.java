88e781e436dd1e11d4952bf82fc02f92687eab17
王保良 <1479930060@qq.com>
2020-01-13 20:45:06 +0800

    trie代码提交

--- src/main/java/com/blanc/datastructure/trie/Trie.java
+++ src/main/java/com/blanc/datastructure/trie/Trie.java
@@ -1,10 +1,13 @@
-package com.blanc.datastructure.tree;
+package com.blanc.datastructure.trie;
 
 import java.util.TreeMap;
 
 /**
  * Trie 字典树,前缀树的实现
- *
+ * 多叉树,微软的通讯录问题
+ * 查询每一个条目的时间复杂度,和字典中一共有多少条目无关,时间复杂度为O(w),w为查询单词的长度!,对于绝大多数的单词,长度都是小于10的
+ * trie: 比如每一个节点(有能力)有26个指向下个节点的指针,a-z26个英文字母,不考虑大小写,不考虑特殊字符,阳春版本
+ * 其实应该是每个节点有若干个指向下个节点的指针
  * @author wangbaoliang
  */
 public class Trie {
@@ -13,17 +16,34 @@ public class Trie {
 
     private int size;
 
+    /**
+     * 定义trie的节点
+     */
     private class Node {
 
+        /**
+         * 标识当前的节点是否是某个单子的结尾(因为一些单词是另一些单词的前缀,所以通过叶子节点判断是否是单词不靠谱)
+         * 如果这个是true,说明到这个节点是个单词
+         */
         public boolean isWord;
 
+        /**
+         * 映射:支持的所有字符中下一个字符以及下一个字符所在的node
+         */
         public TreeMap<Character, Node> next;
 
+        /**
+         * 构造函数
+         * @param isWord
+         */
         public Node(boolean isWord) {
             this.isWord = isWord;
             next = new TreeMap<>();
         }
 
+        /**
+         * 构造函数:默认不是单词
+         */
         public Node() {
             this(false);
         }
