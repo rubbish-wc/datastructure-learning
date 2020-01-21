0941d1b9126399930f623287401a1e4d6751434f
王保良 <1479930060@qq.com>
2020-01-21 17:52:34 +0800

    并查集代码提交

--- src/main/java/com/blanc/datastructure/unionfind/UF.java
+++ src/main/java/com/blanc/datastructure/unionfind/UF.java
@@ -2,21 +2,22 @@ package com.blanc.datastructure.unionfind;
 
 /**
  * 并查集接口
- *
+ * 并查集:一种很不一样的树结构,可以非常高效的解决连接问题
+ * 连接问题
+ * 适用于网络中节点间的连接状态:网络是个抽象的状态,比如用户之间形成的网络
+ * 数学中的集合类的实现,路径问题
  * @author wangbaolinag
  */
 public interface UF {
 
     /**
      * 两个元素是否是可以连接的
-     *
      * @return
      */
     boolean isConnected(int p, int q);
 
     /**
      * 将两个元素连接在一起
-     *
      * @param p
      * @param q
      */
@@ -24,7 +25,6 @@ public interface UF {
 
     /**
      * 获取并查集大小
-     *
      * @return
      */
     int getSize();
---
+++ src/main/java/com/blanc/datastructure/unionfind/UnionFind1.java
@@ -0,0 +1,20 @@
+package com.blanc.datastructure.unionfind;
+
+public class UnionFind1 implements UF {
+
+
+    @Override
+    public boolean isConnected(int p, int q) {
+        return false;
+    }
+
+    @Override
+    public void unionElement(int p, int q) {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+}
