67afdd0d3ebb7593c0dbf7873f5c113ea574268b
王保良 <1479930060@qq.com>
2020-06-14 21:15:33 +0800

    bugfix

--- src/main/java/com/blanc/algorithm/sort/quicksort/QuickSort2.java
+++ src/main/java/com/blanc/algorithm/sort/quicksort/QuickSort2.java
@@ -2,7 +2,17 @@ package com.blanc.algorithm.sort.quicksort;
 
 public class QuickSort2 {
 
-    private void quickSort(int[] array, int begin, int end) {
+    public static void main(String[] args) {
+        int[] array = new int[]{
+                3, 8, 5, 9, 2, 6, 1, 4, 12, 74, 34, 978, 45, 23
+        };
+        quickSort(array, 0, array.length - 1);
+        for (int i = 0 ; i < array.length ; i++){
+            System.out.println(array[i]);
+        }
+    }
+
+    private static void quickSort(int[] array, int begin, int end) {
         //递归终止条件,即pivot不能再细分数组
         if (begin >= end) {
             return;
@@ -23,12 +33,12 @@ public class QuickSort2 {
      * @param end
      * @return
      */
-    private int partition(int[] array, int begin, int end) {
+    private static int partition(int[] array, int begin, int end) {
         int pivot = end;
         int counter = begin;
         //谨记 counter 其实就是过会的pivot,小于的话counter后移,大于的话下个循环要到counter的右边去
         for (int i = begin; i < end; i++) {
-            if (array[i] < pivot) {
+            if (array[i] < array[pivot]) {
                 //交换的逻辑一定是上个循环发现大于,counter没动,然后下个周期交换counter和i
                 //正常的小于这个逻辑不会有影响,因为counter和i是同一个位置
                 int temp = array[counter];
@@ -38,9 +48,9 @@ public class QuickSort2 {
             }
         }
         //循环完毕后,交换counter和pivot,并返回counter作为pivot
-        int temp = array[counter];
-        array[counter] = array[pivot];
-        array[pivot] = counter;
+        int temp = array[pivot];
+        array[pivot] = array[counter];
+        array[counter] = temp;
         return counter;
     }
 }
